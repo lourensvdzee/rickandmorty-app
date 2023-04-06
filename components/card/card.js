e9807336e851baa451e093c040b76374d2adf336
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-06 12:01:00 +0200

    Finish creating card element with JS function

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,28 @@
-export function createCharacterCard() {}
+
+import { cardContainer } from "./index.js"
+export function createCharacterCard(character) {
+    
+ const cardItem = document.createElement("li");
+ cardItem.innerHTML = ` <div class="card__image-container">
+ <img
+   class="card__image"
+   src=${character.results.origin.url}
+   alt="Rick Sanchez"
+ />
+ <div class="card__image-gradient"></div>
+</div>
+<div class="card__content">
+ <h2 class="card__title">${character.results.name}</h2>
+ <dl class="card__info">
+   <dt class="card__info-title">Status</dt>
+   <dd class="card__info-description">${character.results.status}</dd>
+   <dt class="card__info-title">Type</dt>
+   <dd class="card__info-description"></dd>
+   <dt class="card__info-title">Occurrences</dt>
+   <dd class="card__info-description">${character.episode.length}</dd>
+ </dl>
+</div>
+`
+cardContainer.append(cardItem);
+
+}
